b0b9f0bddc83c514fe29d92d0631a0351f475e80
Keks <keks@htmlacademy.ru>
2023-04-21 22:18:08 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/script.js
@@ -0,0 +1,1 @@
+var navMain=document.querySelector(".main-nav__menu"),navToggle=document.querySelector(".main-nav__toggle");navToggle.addEventListener("click",(function(){navMain.classList.toggle("show"),navToggle.classList.toggle("show")}));
