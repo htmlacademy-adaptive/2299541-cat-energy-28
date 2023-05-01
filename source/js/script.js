1bcc04a154997525957a2c2175b749fc86d02a45
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2023-05-01 16:58:02 +0300

    правит работу меню без скрипта

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
+var nav = document.querySelector('.main-header');
 var navMain = document.querySelector('.main-nav__menu');
 var navToggle = document.querySelector('.main-nav__toggle');
 
+nav.classList.remove("main-header--no-js");
 navToggle.addEventListener('click', function () {
   navMain.classList.toggle('show');
   navToggle.classList.toggle('show');
